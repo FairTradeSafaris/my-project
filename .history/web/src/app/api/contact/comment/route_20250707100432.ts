283f6926ce8151314a74f9168818743efb2f4e91
FTS <devon@fairtradesafaris.com>
2025-07-08 13:30:18 +0200

    Update .gitignore and remove .history files

--- .history/web/src/app/api/contact/comment/route_20250707100432.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST(req: Request) {
-  console.log("✅ Hit the /api/comment route!");
-
-  return NextResponse.json({ message: "Ping received!" }, { status: 200 });
-}

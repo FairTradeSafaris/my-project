6d1b0361d2ec2d7b04a9ef93a53dd7b4a4e0b87a
FTS <devon@fairtradesafaris.com>
2025-08-03 11:43:42 +0200

    Limit Clerk middleware to /client-home for debugging

--- web/middleware.ts
+++ web/middleware.ts
@@ -3,9 +3,5 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
-  matcher: [
-    "/((?!.*\\..*|_next).*)", // All routes except static files
-    "/", // Home
-    "/client-home(.*)", // Client portal
-  ],
+  matcher: ["/client-home(.*)"],
 };

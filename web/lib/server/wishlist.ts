65d94b5f452e0e7080f296c65d0ce099dc45a929
FTS <devon@fairtradesafaris.com>
2025-08-30 09:23:03 +0200

    update Clerk

--- web/lib/server/wishlist.ts
+++ web/lib/server/wishlist.ts
@@ -1,14 +1,12 @@
 "use server";
 
-import { clerkClient } from "@clerk/clerk-sdk-node";
+import clerkClient from "@clerk/clerk-sdk-node";
 
 export async function updateWishlist(userId: string, wishlist: string[]) {
-  // Update the user's public metadata with the new wishlist
   await clerkClient.users.updateUser(userId, {
     publicMetadata: { wishlist },
   });
 
-  // Retrieve updated metadata to confirm and return it
   const updatedUser = await clerkClient.users.getUser(userId);
   return updatedUser.publicMetadata?.wishlist || [];
 }
